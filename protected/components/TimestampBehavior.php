72f0c399bdd78869a2e09fd977a063ba7e9be71e
Florian Fackler <florian.fackler@mintao.com>
2012-05-10 23:12:12 +0200

    Added update timestamp for post model

--- protected/components/TimestampBehavior.php
+++ protected/components/TimestampBehavior.php
@@ -2,11 +2,22 @@
 class TimestampBehavior extends CActiveRecordBehavior
 {
     public $createColumnName = 'created';
+    public $updateColumnName = 'updates';
+
+    /**
+     * @var boolean Also set a timestamp for update column if record is created
+     */
+    public $setUpdateOnCreate = false;
 
     public function beforeSave(CModelEvent $event)
     {
         $model = $event->sender;
+        if (true === $model->isNewRecord) {
             $model->{$this->createColumnName} = time();
+        }
+        $model->{$this->updateColumnName} = time();
+        if (false === $this->setUpdateOnCreate && true === $model->isNewRecord) {
+            $model->{$this->updateColumnName} = null;
         }
         return parent::beforeSave($event);
     }
